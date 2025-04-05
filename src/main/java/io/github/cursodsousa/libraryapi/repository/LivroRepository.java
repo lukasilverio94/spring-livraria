0ca08fdc62fb2878fdbe6f7ffb80018fce66eb69
= <lukasilverio94@gmail.com>
2025-04-05 22:03:28 +0200

    add: Query Methods on LivroRepository

--- src/main/java/io/github/cursodsousa/libraryapi/repository/LivroRepository.java
+++ src/main/java/io/github/cursodsousa/libraryapi/repository/LivroRepository.java
@@ -1,9 +1,32 @@
 package io.github.cursodsousa.libraryapi.repository;
 
+import io.github.cursodsousa.libraryapi.model.Autor;
 import io.github.cursodsousa.libraryapi.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 public interface LivroRepository extends JpaRepository<Livro, UUID> {
+
+    // Query Method -> usar findByNomeDaPropriedade em CaseAssim.
+    // select * from livro where id_autor = ?
+    List<Livro> findByAutor(Autor autor);
+
+    // select * from livro where titulo = ?
+    List<Livro> findByTitulo(String titulo);
+
+    // select * from livro where isbn = ?;
+    List<Livro> findByIsbn(String isbn);
+
+    // select * from livro where titulo = ? AND preco = ?
+    List<Livro> findByTituloAndPreco(String titulo, BigDecimal preco);
+
+    // select * from livro where titulo = ? OR isbn = ?
+    List<Livro> findByTituloOrIsbn(String titulo, String isbn);
+
+    // select * from livro where data_publicacao between ? and ?
+    List<Livro> findByDataPublicacaoBetween(LocalDate inicio, LocalDate fim);
 }
