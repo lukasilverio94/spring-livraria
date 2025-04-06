894e70e106e43a50f10a1f0b943532209f588d7a
= <lukasilverio94@gmail.com>
2025-04-06 10:52:53 +0200

    test: implement AutorRepository tests

--- src/test/java/io/github/cursodsousa/libraryapi/repository/AutorRepositoryTest.java
+++ src/test/java/io/github/cursodsousa/libraryapi/repository/AutorRepositoryTest.java
@@ -1,11 +1,15 @@
 package io.github.cursodsousa.libraryapi.repository;
 
 import io.github.cursodsousa.libraryapi.model.Autor;
+import io.github.cursodsousa.libraryapi.model.GeneroLivro;
+import io.github.cursodsousa.libraryapi.model.Livro;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -14,60 +18,106 @@ import java.util.UUID;
 public class AutorRepositoryTest {
 
     @Autowired
-    AutorRepository repository;
+    AutorRepository autorRepository;
+
+    @Autowired
+    LivroRepository livroRepository;
 
     @Test
-    public void salvarTest(){
+    public void salvarTest() {
         Autor autor = new Autor();
         autor.setNome("Juan");
         autor.setNacionalidade("Colombian");
         autor.setDataNascimento(LocalDate.of(1951, 1, 31));
 
-        var autorSalvo = repository.save(autor);
+        var autorSalvo = autorRepository.save(autor);
         System.out.println("Autor Salvo: " + autorSalvo);
     }
 
     @Test
-    public void atualizarTest(){
+    public void atualizarTest() {
         var id = UUID.fromString("2449f4e4-ee1a-4a71-8aa3-e9d46306fe8a");
 
-        Optional<Autor> possivelAutor = repository.findById(id);
+        Optional<Autor> possivelAutor = autorRepository.findById(id);
 
-        if(possivelAutor.isPresent()){
+        if (possivelAutor.isPresent()) {
 
-            Autor autorEncontrado =  possivelAutor.get();
+            Autor autorEncontrado = possivelAutor.get();
             System.out.println("Dados do Autor:");
             System.out.println(autorEncontrado);
 
             autorEncontrado.setDataNascimento(LocalDate.of(1960, 1, 30));
 
-            repository.save(autorEncontrado);
+            autorRepository.save(autorEncontrado);
 
         }
     }
 
     @Test
-    public void listarTest(){
-        List<Autor> lista = repository.findAll();
+    public void listarTest() {
+        List<Autor> lista = autorRepository.findAll();
         lista.forEach(System.out::println);
     }
 
     @Test
-    public void countTest(){
-        System.out.println("Contagem de autores: " + repository.count());
+    public void countTest() {
+        System.out.println("Contagem de autores: " + autorRepository.count());
     }
 
     @Test
-    public void deletePorIdTest(){
+    public void deletePorIdTest() {
         var id = UUID.fromString("2449f4e4-ee1a-4a71-8aa3-e9d46306fe8a");
-        repository.deleteById(id);
+        autorRepository.deleteById(id);
     }
 
     @Test
-    public void deleteTest(){
+    public void deleteTest() {
         var id = UUID.fromString("abc082bf-1d23-4767-b3d9-9f322856ca6a");
-        var maria = repository.findById(id).get();
-        repository.delete(maria);
+        var maria = autorRepository.findById(id).get();
+        autorRepository.delete(maria);
     }
 
+    @Test
+    void salvarAutorComLivrosTest() {
+        Autor autor = new Autor();
+        autor.setNome("Antonia");
+        autor.setNacionalidade("Espanhola");
+        autor.setDataNascimento(LocalDate.of(1970, 8, 25));
+
+        Livro livro = new Livro();
+        livro.setIsbn("124512-84874");
+        livro.setPreco(BigDecimal.valueOf(100));
+        livro.setGenero(GeneroLivro.PROGRAMACAO);
+        livro.setTitulo("Entendendo Algoritmos");
+        livro.setDataPublicacao(LocalDate.of(1980, 1, 2));
+        livro.setAutor(autor);
+
+        Livro livro2 = new Livro();
+        livro2.setIsbn("9837737-84874");
+        livro2.setPreco(BigDecimal.valueOf(100));
+        livro2.setGenero(GeneroLivro.PROGRAMACAO);
+        livro2.setTitulo("Java OOP Programming");
+        livro2.setDataPublicacao(LocalDate.of(2001, 3, 5));
+        livro2.setAutor(autor);
+
+        autor.setLivros(new ArrayList<>());
+        autor.getLivros().add(livro);
+        autor.getLivros().add(livro2);
+
+        autorRepository.save(autor);
+
+//        livroRepository.saveAll(autor.getLivros());
+    }
+
+    @Test
+    void listarLivrosAutor() {
+        var id = UUID.fromString("8d12263e-34c3-454d-ae9c-ddb1ffb25eb3");
+        var autor = autorRepository.findById(id).get();
+
+        // buscar os livros do autor com o query method (no LivroRepository)
+        List<Livro> livrosLista = livroRepository.findByAutor(autor);
+        autor.setLivros(livrosLista);
+
+        autor.getLivros().forEach(System.out::println);
+    }
 }
