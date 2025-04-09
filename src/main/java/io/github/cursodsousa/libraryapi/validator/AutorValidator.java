eadf9df38201b72565a38baa540e3be711b69e11
= <lukasilverio94@gmail.com>
2025-04-09 16:18:09 +0200

    validator: create and update autor validator

---
+++ src/main/java/io/github/cursodsousa/libraryapi/validator/AutorValidator.java
@@ -0,0 +1,44 @@
+package io.github.cursodsousa.libraryapi.validator;
+
+import io.github.cursodsousa.libraryapi.exceptions.RegistroDuplicadoException;
+import io.github.cursodsousa.libraryapi.model.Autor;
+import io.github.cursodsousa.libraryapi.repository.AutorRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class AutorValidator {
+
+    private AutorRepository repository;
+
+    public AutorValidator(AutorRepository repository) {
+        this.repository = repository;
+    }
+
+    public void validar(Autor autor) {
+        if (existeAutorCadastrado(autor)) {
+            throw new RegistroDuplicadoException("Autor já cadastrado");
+        }
+    }
+
+    // auxiliar method
+    private boolean existeAutorCadastrado(Autor autor) {
+
+        Optional<Autor> autorEncontrado = repository.findByNomeAndDataNascimentoAndNacionalidade(
+                // dados do autor que queremos cadastrar
+                autor.getNome(),
+                autor.getDataNascimento(),
+                autor.getNacionalidade()
+        );
+
+        System.out.println("Autor encontrado: " + autorEncontrado);
+        // cadastrar esse autor agora, um novo autor
+        if (autor.getId() == null) {
+            return autorEncontrado.isPresent(); // ve se tem um autor com os dados passados acima
+        }
+
+        return autorEncontrado.filter(value -> !autor.getId().equals(value.getId())).isPresent();
+
+    }
+}
