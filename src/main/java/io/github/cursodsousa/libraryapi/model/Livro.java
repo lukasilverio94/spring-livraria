c17422bfb0b1c516509a2430ee831f84dd9bb1ed
= <lukasilverio94@gmail.com>
2025-04-11 15:14:35 +0200

    add: Validate register book with Bean Validation

--- src/main/java/io/github/cursodsousa/libraryapi/model/Livro.java
+++ src/main/java/io/github/cursodsousa/libraryapi/model/Livro.java
@@ -6,12 +6,18 @@ import lombok.ToString;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.UUID;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Table(name = "livro")
 @Data
 @ToString(exclude = "autor")
+@EntityListeners(AuditingEntityListener.class)
 public class Livro {
 
     @Id
@@ -28,7 +34,7 @@ public class Livro {
     @Column(name = "data_publicacao")
     private LocalDate dataPublicacao;
 
-    @Enumerated(EnumType.STRING )
+    @Enumerated(EnumType.STRING)
     @Column(name = "genero", nullable = false)
     private GeneroLivro genero;
 
@@ -38,5 +44,16 @@ public class Livro {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_autor")
     private Autor autor;
+
+    @CreatedDate
+    @Column(name = "data_cadastro")
+    private LocalDateTime dataCadastro;
+
+    @LastModifiedDate
+    @Column(name = "data_atualizacao")
+    private LocalDateTime dataAtualizacao;
+
+    @Column(name = "id_usuario")
+    private UUID idUsuario;
 
 }
