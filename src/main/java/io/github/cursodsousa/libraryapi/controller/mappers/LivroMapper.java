09dc51da2bd8412c1c89d8a1f4ae8d9b1c9131c5
lukasilverio <lukasilverio94@gmail.com>
2025-04-15 14:03:35 +0200

    add: Get Book By ID, using mapper and DTO

--- src/main/java/io/github/cursodsousa/libraryapi/controller/mappers/LivroMapper.java
+++ src/main/java/io/github/cursodsousa/libraryapi/controller/mappers/LivroMapper.java
@@ -1,13 +1,14 @@
 package io.github.cursodsousa.libraryapi.controller.mappers;
 
 import io.github.cursodsousa.libraryapi.controller.dto.CadastroLivroDTO;
+import io.github.cursodsousa.libraryapi.controller.dto.ResultadoPesquisaLivroDTO;
 import io.github.cursodsousa.libraryapi.model.Livro;
 import io.github.cursodsousa.libraryapi.repository.AutorRepository;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = AutorMapper.class)
 public abstract class LivroMapper {
 
     @Autowired
@@ -18,4 +19,7 @@ public abstract class LivroMapper {
             expression = "java( autorRepository.findById(dto.idAutor()).orElse(null) )"
     )
     public abstract Livro toEntity(CadastroLivroDTO dto);
+
+    public abstract ResultadoPesquisaLivroDTO toDTO(Livro livro);
+
 }
--- src/main/java/io/github/cursodsousa/libraryapi/service/LivroService.java
+++ src/main/java/io/github/cursodsousa/libraryapi/service/LivroService.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Service;
 import io.github.cursodsousa.libraryapi.repository.LivroRepository;
 import lombok.RequiredArgsConstructor;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 public class LivroService {
@@ -15,4 +18,9 @@ public class LivroService {
     public Livro salvar(Livro livro) {
         return repository.save(livro);
     }
+
+    public Optional<Livro> obterPorId(UUID id){
+        return repository.findById(id);
+    }
+
 }
